3c41eb5314c1d5c58dbfb1646114f517dbbc61d0
Denys Kuz <denis.kuz96@gmail.com>
2022-09-18 15:39:32 +0300

    HW: Movies

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </StrictMode>
